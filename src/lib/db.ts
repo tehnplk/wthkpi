f9cffc5459ba7208c29f65c6577f90d01748f1d7
tehnplk <tehnplk@gmail.com>
2026-05-11 12:23:25 +0700

    Add socketPath undefined to force TCP connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,6 +8,7 @@ const db: Knex = knex({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME || "wthkpi",
+    socketPath: undefined,
   },
   pool: { min: 1, max: 10 },
 });
